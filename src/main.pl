72571441d33884126838d43acfb4c9258917ce6d
darkclown <darkclown@life-hack.org>
2012-11-20 20:38:29 +0900

    fixed license notice

--- src/main.pl
+++ src/main.pl
@@ -2,18 +2,18 @@
 
 #Copyright (c) 2012 Johan Gustavsson <johan@life-hack.org>
 #
-#Hive-api is free software: you can redistribute it and/or modify
+#Twitter-HDFS is free software: you can redistribute it and/or modify
 #it under the terms of the GNU General Public License as published by
 #the Free Software Foundation, either version 3 of the License, or
 #(at your option) any later version.
 #
-#Hive-api is distributed in the hope that it will be useful,
+#Twitter-HDFS is distributed in the hope that it will be useful,
 #but WITHOUT ANY WARRANTY; without even the implied warranty of
 #MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #GNU General Public License for more details.
 #
 #You should have received a copy of the GNU General Public License
-#along with Hive-api.  If not, see <http://www.gnu.org/licenses/>.
+#along with Twitter-HDFS.  If not, see <http://www.gnu.org/licenses/>.
 
 use encoding "utf-8";
 use strict;
